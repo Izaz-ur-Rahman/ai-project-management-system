68ae47a6aa4d08e076c6ec397068de73b45eff3e
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-08-07 22:56:03 +0500

    feat: initialize database before starting server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const app = require("./src/app");
 const env = require("./src/config/env");
+const connectDatabase = require("./src/config/database");
 
-app.listen(env.port, () => {
-    console.log(
-        `Server running on port ${env.port} in ${env.nodeEnv} mode`
-    );
-});
+const startServer = async () => {
+    await connectDatabase();
+
+    app.listen(env.port, () => {
+        console.log(
+            `Server running on port ${env.port} in ${env.nodeEnv} mode`
+        );
+    });
+};
+
+startServer();
