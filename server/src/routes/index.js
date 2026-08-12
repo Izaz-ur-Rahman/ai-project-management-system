a2bbee2864727e9149218227b84045c90eafd82e
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2026-08-12 22:00:22 +0500

    feat: add role based authorization middleware

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./authRoutes");
+const authorize = require("../middleware/roleMiddleware");
+const authenticate = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/health", (req, res) => {
@@ -17,4 +19,19 @@ router.get("/health", (req, res) => {
 });
 router.use("/auth", authRoutes);
 
+router.get(
+    "/admin-test",
+    authenticate,
+    authorize("admin"),
+    (req, res) => {
+        return res.status(200).json({
+            success: true,
+            message: "Admin access granted",
+            data: {
+                user: req.user.name,
+                role: req.user.role,
+            },
+        });
+    }
+);
 module.exports = router;
